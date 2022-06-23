1c5f5e249ca6ee6bf4c4d54ef18342fe2197123f
th3g3ntl3boy <afick98@gmail.com>
2022-06-23 12:23:42 +0800

    tes

---
+++ config/plugins.js
@@ -0,0 +1,15 @@
+
+module.exports = ({ env }) => ({
+    //
+    graphql: {
+      config: {
+        endpoint: "/graphql",
+        shadowCRUD: true,
+        playgroundAlways: true,
+        depthLimit: 100,
+        apolloServer: {
+          tracing: false,
+        },
+      }
+    },
+  });
