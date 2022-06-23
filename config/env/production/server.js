7072eafd5595c6be29dbc9def258ad8a7331e6e3
th3g3ntl3boy <afick98@gmail.com>
2022-06-23 12:06:28 +0800

    bug

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -2,6 +2,12 @@ module.exports = ({ env }) => ({
     proxy: true,
     url: env('MY_HEROKU_URL'),
     app: { 
-      keys: env.array('APP_KEYS')
+      keys: env.array('APP_KEYS',[
+
+        "APPKEYS1==",
+        "APPKEYS2==",
+        "APPKEYS3==",
+        "APPKEYS4==",
+      ])
     },
   })
