cbce10c5d513ec84bef70d57abf3c52b10400d1b
th3g3ntl3boy <afick98@gmail.com>
2022-07-01 15:37:54 +0800

    add comment interact

---
+++ src/api/commentlike/controllers/commentlike.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  commentlike controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::commentlike.commentlike');
---
+++ src/api/commentlike/routes/commentlike.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * commentlike router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::commentlike.commentlike');
